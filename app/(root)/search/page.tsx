b60dc0776e916efc01ae4c2c071dd5e03f4587a0
orlando-guy <guichardorlando@gmail.com>
2024-04-06 22:32:47 +0100

    style(search): remove useless commented-out lines

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,6 @@
 import UserCard from "@/components/cards/UserCard"
 import { fetchUsers, fetchUser } from "@/lib/actions/user.action"
 import { currentUser } from "@clerk/nextjs"
-// import Image from "next/image"
 import { redirect } from "next/navigation"
 
 export default async function SearchPage() {
@@ -23,7 +22,6 @@ export default async function SearchPage() {
     return (
         <section>
             <h1 className="head-text">Search</h1>
-            {/* TODO: Here's where I took a break, a 04:30:41 */}
             <div className="mt-14 flex flex-col gap-9">
                 {result.users.length === 0 ? (
                     <p className="no-result">No users</p>
