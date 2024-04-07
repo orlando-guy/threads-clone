1c17b072b2b153251ff1b17d853120288a50584b
orlando-guy <guichardorlando@gmail.com>
2024-04-07 15:08:49 +0100

    feat(community): craft the fundation of the community part

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,8 @@
+export default function ComunitiesPage() {
+    // TODO: took a break at 05:07:38
+    return (
+        <section>
+            <h1 className="head-text">Comunities</h1>
+        </section>
+    )
+}
